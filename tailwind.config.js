6f00ac19eb8142bf9c888e92fb1f4bd58efe628c
henriqdsvlima <henrique.dsvlima@gmail.com>
2023-11-03 14:13:00 -0300

    criando header e call to action

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'portifolio-text-white': '#fffcf2',
+        'portfolio-text': '#ccc5b9',
+        'bg-portfolio': '#403d39',
+        'bg-portfolio-darker': '#252422',
+        'hover-color': '#eb5e28',
+      }
+    }
   },
   plugins: [],
 }
