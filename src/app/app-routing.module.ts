0d696be9a6770edd1744386fde76ff8dd031a2c6
Henrique <henrique.dsvlima@gmail.com>
2023-11-04 16:45:37 -0300

    customizando header, cta e footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { ExperienceComponent } from './components/pages/experience/experience.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component:HomeComponent,
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent
   }
 ];
 
